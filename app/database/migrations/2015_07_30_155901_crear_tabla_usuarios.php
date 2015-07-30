da6b3a279c224d3be6f024f854e01eaa4b07516b
Alberto Leal <albertoleal87@hotmail.com>
2015-07-30 11:49:33 -0500

    Se crea la tabla usuarios y se mapea para login

---
+++ app/database/migrations/2015_07_30_155901_crear_tabla_usuarios.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuarios extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('usuarios', function($table){
+			$table->increments('id');
+			$table->boolean('estatus')->default(1);
+			$table->string('nombre');
+			$table->string('apellido_paterno');
+			$table->string('apellido_materno');
+			$table->string('telefono');
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->string('remember_token');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('usuarios');
+	}
+
+}
