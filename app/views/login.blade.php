76019a7e7ab270213cf68347cb4a00c65cf60a9b
Alberto Leal <albertoleal87@hotmail.com>
2015-08-26 15:24:57 -0500

    Se carga bootstrap y se implementa en el login view

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,11 +1,55 @@
-{{ Form::open( ['url'=>'login', 'method'=>"post"] ) }}
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
 
-    {{ Form::email( 'email', null, ['placeholder'=>'example@mail.com'] ) }}
-    {{ Form::password( 'password', ['placeholder'=>'password'] ) }}
-    {{ Form::submit( 'Login' ) }}
+		<title>Serverus</title>
 
-    @if(Session::has('message'))
-        {{ Session::get('message') }}
-    @endif
-    
-{{ Form::close() }}
+		{{ HTML::style('public/css/bootstrap.min.css') }}
+		{{ HTML::style('public/css/serverus.css') }}
+
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4">
+					<div class="login-panel panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title ">Please Sign In</h3>
+						</div>
+
+						<div class="panel-body">
+							{{ Form::open( ['url'=>'login', 'method'=>"post"] ) }}
+							<fieldset>
+								
+								<div class="form-group">
+									{{ Form::email( 'email', null, ['class'=>'form-control ','placeholder'=>'E-mail'] ) }}
+								</div>
+								
+								<div class="form-group">
+									{{ Form::password( 'password', ['class'=>'form-control','placeholder'=>'Password'] ) }}
+								</div>
+
+								{{ Form::submit( 'Login' , ['class'=>'btn btn-lg btn-success btn-block']) }}
+
+								@if(Session::has('message'))
+									{{ Session::get('message') }}
+								@endif
+
+							</fieldset>
+							{{ Form::close() }}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		{{ HTML::script('public/js/bootstrap.min.js') }}
+
+	</body>
+</html>
