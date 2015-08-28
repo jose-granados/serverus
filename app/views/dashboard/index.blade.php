2e94188f09adef0ea5a767c6bd661a9cfd8d708c
Alberto Leal <albertoleal87@hotmail.com>
2015-08-28 17:44:41 -0500

    Ajustes en ligas que apuntaban a la otra pagina

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -19,7 +19,7 @@
                     </div>
                 </div>
             </div>
-            <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#">
+            <a href="{{ URL::to('/') }}">
                 <div class="panel-footer fill-blue">
                     <span class="pull-left">Ver detalle</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -41,7 +41,7 @@
                     </div>
                 </div>
             </div>
-            <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#">
+            <a href="{{ URL::to('/') }}">
                 <div class="panel-footer fill-green">
                     <span class="pull-left">Ver detalle</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -63,7 +63,7 @@
                     </div>
                 </div>
             </div>
-            <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#">
+            <a href="{{ URL::to('/') }}">
                 <div class="panel-footer fill-yellow">
                     <span class="pull-left">Ver detalle</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -85,7 +85,7 @@
                     </div>
                 </div>
             </div>
-            <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#">
+            <a href="{{ URL::to('/') }}">
                 <div class="panel-footer fill-red">
                     <span class="pull-left">Ver detalle</span>
                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -122,47 +122,47 @@
             <!-- /.panel-heading -->
             <div class="panel-body">
                 <div class="list-group">
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-comment fa-fw"></i> New Comment
                         <span class="pull-right text-muted small"><em>4 minutes ago</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-twitter fa-fw"></i> 3 New Followers
                         <span class="pull-right text-muted small"><em>12 minutes ago</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-envelope fa-fw"></i> Message Sent
                         <span class="pull-right text-muted small"><em>27 minutes ago</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-tasks fa-fw"></i> New Task
                         <span class="pull-right text-muted small"><em>43 minutes ago</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-upload fa-fw"></i> Server Rebooted
                         <span class="pull-right text-muted small"><em>11:32 AM</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-bolt fa-fw"></i> Server Crashed!
                         <span class="pull-right text-muted small"><em>11:13 AM</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-warning fa-fw"></i> Server Not Responding
                         <span class="pull-right text-muted small"><em>10:57 AM</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-shopping-cart fa-fw"></i> New Order Placed
                         <span class="pull-right text-muted small"><em>9:49 AM</em>
                         </span>
                     </a>
-                    <a href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#" class="list-group-item">
+                    <a href="{{ URL::to('/') }}" class="list-group-item">
                         <i class="fa fa-money fa-fw"></i> Payment Received
                         <span class="pull-right text-muted small"><em>Yesterday</em>
                         </span>
