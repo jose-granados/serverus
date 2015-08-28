16d8488f7141d5807d2bb4fd63ca3b46699b27f1
Epifanio Hernandez <epifanio.hdz91@gmail.com>
2015-08-28 16:28:53 -0500

    Limpieza de codifo de css y js

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -1,64 +1,30 @@
 <!DOCTYPE html>
-<!-- saved from url=(0075)http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html -->
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Serverus</title>
-
 
     <!-- Bootstrap Core CSS -->
     {{ HTML::style('public/css/bootstrap.min.css') }}
+    {{ HTML::style('public/css/font-awesome.min.css') }}
+    {{ HTML::style('public/css/dataTables.bootstrap.min.css') }}
+    {{ HTML::style('public/css/serverus.css') }}
+
     {{ HTML::script('public/js/jquery.min.js') }}
     {{ HTML::script('public/js/bootstrap.min.js') }}
-    {{ HTML::script('public/js/metisMenu.min.js') }}
-    {{ HTML::script('public/js/raphael-min.js') }}
-    <!--{{ HTML::script('public/js/morris.min.js') }}
-    {{ HTML::script('public/js/morris-data.js') }}-->
-    {{ HTML::script('public/js/sb-admin-2.js') }}
+    {{ HTML::script('public/js/metisMenu.min.js') }}    
     {{ HTML::script('public/js/jquery.dataTables.min.js') }}
-    {{ HTML::script('public/js/dataTables.bootstrap.min.js') }}
-    {{ HTML::style('public/css/dataTables.bootstrap.min.css') }}
-
+    {{ HTML::script('public/js/dataTables.bootstrap.min.js') }}   
     {{ HTML::script('public/js/bootbox.min.js') }}
-
     {{ HTML::script('public/js/serverus.js') }}
-
-
-    <!-- MetisMenu CSS -->
-    <link href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Timeline CSS -->
-    <link href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/dist/css/timeline.css" rel="stylesheet">
-
-
-
-    <!-- Morris Charts CSS -->
-    <link href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/bower_components/morrisjs/morris.css" rel="stylesheet">
-    <!-- Custom Fonts -->
-
-    {{ HTML::style('public/css/serverus.css') }}
-    {{ HTML::style('public/css/sb-admin-2.css') }}
-   {{ HTML::style('public/css/font-awesome.min.css') }}
-<link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8" />
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-<style type="text/css"></style></head>
-
+</head>
 <body>
-
     <div id="wrapper">
-
         <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
@@ -70,8 +36,6 @@
                 </button>
                 <a class="navbar-brand" href="./SB Admin 2 - Bootstrap Admin Theme_files/SB Admin 2 - Bootstrap Admin Theme.html"><img class="logo" src="public/img/logo02.png"></a>
             </div>
-            <!-- /.navbar-header -->
-
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#">
@@ -121,11 +85,7 @@
                             </a>
                         </li>
                     </ul>
-                    <!-- /.dropdown-messages -->
                 </li>
-                <!-- /.dropdown -->
-
-                <!-- /.dropdown -->
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="http://ironsummitmedia.github.io/startbootstrap-sb-admin-2/pages/index.html#">
                         <i class="fa fa-map-marker fa-fw"></i>
@@ -183,19 +143,13 @@
                             </a>
                         </li>
                     </ul>
-                    <!-- /.dropdown-alerts -->
                 </li>
-                <!-- /.dropdown -->
                 <li class="dropdown">
                     <a href="{{ URL::to('logout') }}">
                         <i class="fa fa-power-off fa-fw"></i>
                     </a>
-                    <!-- /.dropdown-user -->
                 </li>
-                <!-- /.dropdown -->
             </ul>
-            <!-- /.navbar-top-links -->
-
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse collapse">
                     <ul class="nav in" id="side-menu">
@@ -222,9 +176,7 @@
                         </li>
                     </ul>
                 </div>
-                <!-- /.sidebar-collapse -->
             </div>
-            <!-- /.navbar-static-side -->
         </nav>
 
         <div id="page-wrapper" style="min-height: 780px;">
@@ -232,13 +184,6 @@
         {{ $content  }}
 
         </div>
-        <!-- /#page-wrapper -->
-
     </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-
-
-
-</body></html>
+</body>
+</html>
