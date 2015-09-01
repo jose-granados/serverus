3d77b4f9fbb00ce9006bb62c78c9783b45cee5b3
renzokr <ren_ecs@hotmail.com>
2015-09-01 11:09:03 -0500

    creacion tabla usuarios_servidores

---
+++ app/database/migrations/2015_09_01_160537_crear_tabla_usuarios_servidores.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuariosServidores extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::dropIfExists('usuarios_servidores');
+		Schema::create('usuarios_servidores', function($table)
+		{
+			$table->integer('servidores_id')->unsigned();
+			$table->string('usuario');
+			$table->string('password');
+			$table->foreign('servidores_id')->references('id')->on('servidores');
+			$table->unique( array('servidores_id') );
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('usuarios_servidores');
+	}
+
+}
