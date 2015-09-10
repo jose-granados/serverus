1083945160d9e17a90fcceb53d9c294d30e10eed
mrfreeze <mauricio.garcia@bsdenterprise.com>
2015-09-10 11:53:56 -0500

    acceso denegado

--- app/views/accesso_denegado.blade.php
+++ app/views/accesso_denegado.blade.php
@@ -1,6 +1,7 @@
 <div class="row">
     <div class="col-lg-12">
-        <h4 class="page-header">Acceso denegado</h4>
-    	<div class="alert alert-danger">Lo sentimos pero no puedes acceder a este contenido</div>
+        <h4 class="page-header-denegado">Acceso denegado</h4>
+        <div class="alert alert-danger">Lo sentimos pero no puedes acceder a este contenido</div>
+        <div class="">{{ HTML::image('public/img/pug.gif') }}</div>
     </div>
 </div>
