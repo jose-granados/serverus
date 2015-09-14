e9cd6689dd22600360ace25b988e3fceb9c66545
Alberto Leal <albertoleal87@hotmail.com>
2015-09-14 11:37:09 -0500

    Se cargan configuraciones de email

--- app/database/seeds/ConfiguracionesSeeder.php
+++ app/database/seeds/ConfiguracionesSeeder.php
@@ -4,23 +4,19 @@ class ConfiguracionesSeeder extends Seeder {
 
     public function run(){
 
-    	Configuracion::truncate();
-		Configuracion::insert(array(
-			array(
-				'nombre_configuracion'=>'app.debug',
-				'valor'=>'true',
-				'descripcion'=>'Modo debug (true/false)',
-			),
-			array(
-				'nombre_configuracion'=>'database.log_query',
-				'valor'=>'true',
-				'descripcion'=>'Escribe los querys en el archivo log (true/false)',
-			),
-			array(
-				'nombre_configuracion'=>'app.log_in_console',
-				'valor'=>'true',
-				'descripcion'=>'Imprime el log en consola',
-			),
+		DB::table('configuraciones')->truncate();
+		DB::table('configuraciones')->insert(array(
+			array('nombre_configuracion'=>'app.debug','valor'=>'true','descripcion'=>'Modo debug (debe ser "true" para que esté habilitado) '),
+			array('nombre_configuracion'=>'app.log_in_console','valor'=>'true','descripcion'=>'Muestra en consola lo que se imprime en el archivo de log (debe ser "true" para que esté habilitado)'),
+			array('nombre_configuracion'=>'database.log_query','valor'=>'true','descripcion'=>'Guarda en el log los querys que se ejecutan en base de datos (debe ser "true" para que esté habilitado)'),
+			array('nombre_configuracion'=>'mail.driver','valor'=>'smtp','descripcion'=>'Driver para configuración de correo'),
+			array('nombre_configuracion'=>'mail.host','valor'=>'smtp.gmail.com','descripcion'=>'Host para configuración de correo'),
+			array('nombre_configuracion'=>'mail.port','valor'=>'587','descripcion'=>'Puerto para configuración de correo'),
+			array('nombre_configuracion'=>'mail.encryption','valor'=>'tls','descripcion'=>'Encriptacion para configuración de correo'),
+			array('nombre_configuracion'=>'mail.username','valor'=>'serverus.test@gmail.com','descripcion'=>'Cuenta de correo para envio'),
+			array('nombre_configuracion'=>'mail.password','valor'=>'serverus.test1!','descripcion'=>'Contraseña de la cuenta de correo para envio'),
+			array('nombre_configuracion'=>'mail.from.address','valor'=>'serverus.test@gmail.com','descripcion'=>'Direccion que aparecerá en los correos enviados por la aplicación'),
+			array('nombre_configuracion'=>'mail.from.name','valor'=>'Serverus TEST','descripcion'=>'Nombre que aparecerá en los correos enviados por la aplicación'),
 		));
 
     }
