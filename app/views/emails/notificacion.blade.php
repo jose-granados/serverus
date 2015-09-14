837dde5ee4b08226f0a6e037a6237d1a8f7f648b
Epifanio Hernandez <epifanio.hdz91@gmail.com>
2015-09-14 11:08:23 -0500

    Plantilla de correo

--- app/views/emails/notificacion.blade.php
+++ app/views/emails/notificacion.blade.php
@@ -2,8 +2,47 @@
 <html lang="es">
    <head>
       <meta charset="utf-8">
+        <style type="text/css">
+	        .contenedor_correo {
+	            padding: 40px;
+	        }
+	        .contenedor_correo td {
+	            padding: 10px;
+	        }
+	        .titulo {
+	            background: #f8f8f8 none repeat scroll 0 0;
+	            border: 1px solid #eeeeee;
+	            color: #555;
+	            font-size: 20px;
+	            margin-top: 20px;
+	            padding: 3px;
+	            text-align: center;
+	        }
+	    </style>
    </head>
    <body>
-      <h1></h1>
+        <div class="contenedor_correo">
+        <div class="titulo">Datos de la aplicación</div>
+        <table style="width:100%; border: 1px solid #eeeeee;">
+            <tr>
+                <td><strong>Servidor:</strong></td>
+                <td>$variable</td> 
+            </tr>
+            <tr>
+                <td><strong>Localización:</strong></td>
+                <td>$variable</td> 
+            </tr>
+            <tr>
+                <td><strong>Aplicación:</strong></td>
+                <td>$variable</td> 
+            </tr>
+            <tr>
+                <td><strong>Mensaje:</strong></td>
+                <td>
+                    Este es un mensaje de pruebas para indicar que el servidor tiene una aplicación que no esta funcioanando, vefiricar confuguración.
+                </td>
+            </tr>
+        </table>
+      </div>
    </body>
 </html>
